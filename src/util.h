0e21f4a7fde6a53577f1ca17cea5ab1251fba73e
davidspiv <daspivack@gmail.com>
2025-04-02 21:06:14 -0700

    made sRGB use int for member variables

--- src/util.h
+++ src/util.h
@@ -11,12 +11,14 @@ const CieXyz referenceWhiteD60(0.950470, 1.0, 1.088830);
 
 template <typename ColorT>
 float distEuclideanSquared(const ColorT &a_Color, const ColorT &b_Color) {
-  std::array<float, 3> a_Values = a_Color.getValues;
-  std::array<float, 3> b_Values = b_Color.getValues;
+  using T = decltype(a_Color.values()[0]);
 
-  const float xD = a_Values[0] - b_Values[0];
-  const float yD = a_Values[1] - b_Values[1];
-  const float zD = a_Values[2] - b_Values[2];
+  std::array<T, 3> a_Values = a_Color.getValues;
+  std::array<T, 3> b_Values = b_Color.getValues;
+
+  const T xD = a_Values[0] - b_Values[0];
+  const T yD = a_Values[1] - b_Values[1];
+  const T zD = a_Values[2] - b_Values[2];
   return xD * xD + yD * yD + zD * zD;
 }
 
