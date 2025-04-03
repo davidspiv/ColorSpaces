0dae0d5f25f8e54d5968a913c24ba8c941656291
davidspiv <daspivack@gmail.com>
2025-04-02 17:06:36 -0700

    added more util function definitions

--- src/util.h
+++ src/util.h
@@ -14,10 +14,21 @@ constexpr float kappa = 24389.0 / 27.0;
 float distEuclideanSquared(const LinearRgb &colorA, const LinearRgb &colorB);
 float distEuclideanSquared(const CieXyz &colorA, const CieXyz &colorB);
 float distEuclideanSquared(const CieLab &colorA, const CieLab &colorB);
+// float distEuclideanSquared(const CieLch &colorA, const CieLch &colorB);
 
 float distEuclidean(const LinearRgb &colorA, const LinearRgb &colorB);
 float distEuclidean(const CieXyz &colorA, const CieXyz &colorB);
 float distEuclidean(const CieLab &colorA, const CieLab &colorB);
+// float distEuclidean(const CieLch &colorA, const CieLch &colorB);
+
+// float distCIE76(const CieLab &colorA, const CieLab &colorB);
+// float distCIE76(const CieLch &colorA, const CieLch &colorB);
+
+// float distCIEDE2000(const CieLab &colorA, const CieLab &colorB);
+// float distCIEDE2000(const CieLch &colorA, const CieLch &colorB);
+
+// float distCIE94(const CieLab &colorA, const CieLab &colorB);
+// float distCIE94(const CieLch &colorA, const CieLch &colorB);
 
 std::array<float, 3>
 multiplyMatrix(const std::array<std::array<float, 3>, 3> &matrix,
