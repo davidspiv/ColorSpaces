c525c2d155f40b3faf4e596a37c8fed776735f6c
davidspiv <daspivack@gmail.com>
2025-04-02 21:08:00 -0700

    made sRGB use int for member variables

--- src/main.cpp
+++ src/main.cpp
@@ -14,22 +14,22 @@ int main() {
   const ColorSpace::CieXyz cieXy_A = linearRgb_A.toCieXyz();
   const ColorSpace::CieLab cieLab_A = cieXy_A.toCieLab();
 
-  cieLab_A.print();
+  //   cieLab_A.print();
 
-  //   const ColorSpace::Srgb sRgb_B(0, 201, 0);
-  //   const ColorSpace::LinearRgb linearRgb_B = sRgb_B.toLinearRgb();
-  //   const ColorSpace::CieXyz cieXyz_B = linearRgb_B.toCieXyz();
-  //   const ColorSpace::CieLab cieLab_B = cieXyz_B.toCieLab();
+  const ColorSpace::Srgb sRgb_B(0, 201, 0);
+  const ColorSpace::LinearRgb linearRgb_B = sRgb_B.toLinearRgb();
+  const ColorSpace::CieXyz cieXyz_B = linearRgb_B.toCieXyz();
+  const ColorSpace::CieLab cieLab_B = cieXyz_B.toCieLab();
 
   //   const float distLinear = distEuclideanSquared(linearRgb_A, linearRgb_B);
   //   const float distXyz = distEuclideanSquared(cieXy_A, cieXyz_B);
   //   const float distLab = distEuclideanSquared(cieLab_A, cieLab_B);
 
-  //   const float distLinear = linearRgb_A.distEuclidean(linearRgb_B);
-  //   const float distXyz = cieXy_A.distEuclidean(cieXyz_B);
-  //   const float distLab = cieLab_A.distEuclidean(cieLab_B);
+  const float distLinear = distEuclidean(linearRgb_A, linearRgb_B);
+  const float distXyz = distEuclidean(cieXy_A, cieXyz_B);
+  const float distLab = distEuclidean(cieLab_A, cieLab_B);
 
-  //   std::cout << "linear: " << distLinear << '\n';
-  //   std::cout << "xyz: " << distXyz << '\n';
-  //   std::cout << "lab: " << distLab << '\n';
+  std::cout << "linear: " << distLinear << '\n';
+  std::cout << "xyz: " << distXyz << '\n';
+  std::cout << "lab: " << distLab << '\n';
 }
